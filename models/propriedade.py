bb3b88583c257184821e6cbc04fe980090bbe91e
unknown <holandakassio@hotmail.com>
2022-07-10 12:44:51 -0300

    definindo metodos do model propriedade

--- models/propriedade.py
+++ models/propriedade.py
@@ -4,10 +4,22 @@ class Propriedade():
         self.preco_compra = preco_compra
         self.preco_aluguel = preco_aluguel
         self.proprietario = proprietario
-        
-    def esta_disponivel_compra(self):
-        return self.proprietario is None
     
-    def devolver_propriedade(self):
+    def devolver(self):
         if self.proprietario is not None:
             self.proprietario = None
+            
+    def comprar(self, jogador):
+        if self.proprietario is None:
+            self.proprietario = jogador
+            jogador.saldo -= self.preco_compra
+            return True
+        return False
+    
+    def receber_aluguel(self, jogador):
+        jogador.saldo -= self.preco_aluguel
+        self.proprietario.saldo += self.preco_aluguel
+    
+    def __repr__(self):
+        return self.nome
+            
