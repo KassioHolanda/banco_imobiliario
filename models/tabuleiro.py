575e42082220e56f1e2cbf902a562680c0427867
Kassio Leodido <Leodido#98006627>
2022-07-10 14:03:19 -0300

    definindo os metodos do model tabuleiro

--- models/tabuleiro.py
+++ models/tabuleiro.py
@@ -9,11 +9,17 @@ class Tabuleiro:
     def add_propriedade(self, propriedade):
         self.propriedades.append(propriedade)
     
-    def add_jogador(self, jogador):
-        self.jogadores.append(jogador)
+    def add_jogadores(self, jogadores):
+        for jogador in jogadores:
+            self.jogadores.append(jogador)
         
     def sortear_jogadores(self):
-        random.shufle(self.jogadores)
+        random.shuffle(self.jogadores)
         
     def remover_jogador(self, jogador):
         self.jogadores.remove(jogador)
+
+    def devolver_propriedades_jogador(self, jogador):
+        for propriedade in self.propriedades:
+            if propriedade.proprietario.nome == jogador.nome:
+                propriedade.devolver()
